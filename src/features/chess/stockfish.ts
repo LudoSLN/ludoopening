d8efe4914ed82153ff2a6d944a20c328105c9f48
LudoSLN <ludosln85@gmail.com>
2025-05-26 12:37:51 +0200

    Delete src/features/chess/stockfish.ts

--- src/features/chess/stockfish.ts
+++
@@ -1,9 +0,0 @@
-export function initializeStockfish(): Worker {
-  const stockfish = new Worker('/stockfish.js');
-  
-  // Initialize UCI mode
-  stockfish.postMessage('uci');
-  stockfish.postMessage('isready');
-  
-  return stockfish;
-}
